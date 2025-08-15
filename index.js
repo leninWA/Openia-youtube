1b92d8224774b0530bd1df4bfa79c5ac5add131a
leninWA <soyultragood@gmail.com>
2025-08-14 20:32:38 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+youtube-downloader/
+│
+├─ downloads/                 # Carpeta donde se guardan los archivos descargados
+│
+├─ index.js                   # Archivo principal con la lógica de descarga (MP3 y MP4)
+│
+├─ package.json               # Archivo de dependencias y configuración de Node.js
+│
+├─ package-lock.json          # Archivo que gestiona versiones exactas de dependencias (se genera con npm)
+│
+└─ README.md                  # Documentación del proyecto (descripción, uso, instalación)
